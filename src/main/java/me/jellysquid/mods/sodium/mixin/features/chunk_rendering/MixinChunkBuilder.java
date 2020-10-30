310dd9bd297e5ba2b8f75df0d212e614170f044a
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2020-10-30 02:34:39 -0600

    Fixes + temporary fixes

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/chunk_rendering/MixinChunkBuilder.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/chunk_rendering/MixinChunkBuilder.java
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(ChunkBatcher.class)
 public class MixinChunkBuilder {
     //Shouldn't break but it's broken
-    /*@ModifyVariable(method = "<init>", index = 9, at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayListWithExpectedSize(I)Ljava/util/ArrayList;", remap = false))
+    @ModifyVariable(method = "<init>", index = 8, at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayListWithExpectedSize(I)Ljava/util/ArrayList;", remap = false))
     private int modifyThreadPoolSize(int prev) {
         // Do not allow any resources to be allocated
         return 0;
-    }*/
+    }
 }
