25f35a61592dd3909b7cb6bd0c241f28f1ae0d0d
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-02-13 00:37:39 -0500

    new: Reduce draw calls on loading screen
    Reduces draw calls and overhead. This commit was picked from Lithium as it was later removed in new versions

---
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/fast_loading_screen/MixinLevelLoadingScreen.java
@@ -0,0 +1,121 @@
+package me.jellysquid.mods.sodium.mixin.features.gui.fast_loading_screen;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.client.gui.WorldGenerationProgressTracker;
+import net.minecraft.client.gui.screen.LevelLoadingScreen;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.world.chunk.ChunkStatus;
+import org.spongepowered.asm.mixin.*;
+
+import java.util.IdentityHashMap;
+
+/**
+ * Re-implements the loading screen with considerations to reduce draw calls and other sources of overhead. This can
+ * improve world load times on slower processors with very few cores.
+ */
+@Mixin(LevelLoadingScreen.class)
+public class MixinLevelLoadingScreen {
+    @Mutable
+    @Shadow
+    @Final
+    private static Object2IntMap<ChunkStatus> STATUS_TO_COLOR;
+
+    private static IdentityHashMap<ChunkStatus, Integer> STATUS_TO_COLOR_FAST;
+
+    private static final int NULL_STATUS_COLOR = -16777216;
+    private static final int DEFAULT_STATUS_COLOR = -16772609;
+
+    /**
+     * This implementation differs from vanilla's in the following key ways.
+     * - All tiles are batched together in one draw call, reducing CPU overhead by an order of magnitudes.
+     * - Identity hashing is used for faster ChunkStatus -> Color lookup.
+     * - Colors are stored in unpacked RGBA format so conversion is not necessary every tile draw
+     *
+     * @reason Significantly optimized implementation.
+     * @author JellySquid
+     */
+    @Overwrite
+    public static void drawChunkMap(MatrixStack matrixStack, WorldGenerationProgressTracker tracker, int mapX, int mapY, int mapScale, int mapPadding) {
+        if (STATUS_TO_COLOR_FAST == null) {
+            STATUS_TO_COLOR_FAST = new IdentityHashMap<>(STATUS_TO_COLOR.size());
+            STATUS_TO_COLOR_FAST.put(null, NULL_STATUS_COLOR);
+            STATUS_TO_COLOR.object2IntEntrySet()
+                    .forEach(entry -> STATUS_TO_COLOR_FAST.put(entry.getKey(), entry.getIntValue() | -16777216));
+        }
+
+        Tessellator tessellator = Tessellator.getInstance();
+
+        RenderSystem.enableBlend();
+        RenderSystem.disableTexture();
+        RenderSystem.defaultBlendFunc();
+
+        BufferBuilder buffer = Tessellator.getInstance().getBuffer();
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+
+        int centerSize = tracker.getCenterSize();
+        int size = tracker.getSize();
+
+        int tileSize = mapScale + mapPadding;
+
+        if (mapPadding != 0) {
+            int mapRenderCenterSize = centerSize * tileSize - mapPadding;
+            int radius = mapRenderCenterSize / 2 + 1;
+
+            addRect(buffer, mapX - radius, mapY - radius, mapX - radius + 1, mapY + radius, DEFAULT_STATUS_COLOR);
+            addRect(buffer, mapX + radius - 1, mapY - radius, mapX + radius, mapY + radius, DEFAULT_STATUS_COLOR);
+            addRect(buffer, mapX - radius, mapY - radius, mapX + radius, mapY - radius + 1, DEFAULT_STATUS_COLOR);
+            addRect(buffer, mapX - radius, mapY + radius - 1, mapX + radius, mapY + radius, DEFAULT_STATUS_COLOR);
+        }
+
+        int mapRenderSize = size * tileSize - mapPadding;
+        int mapStartX = mapX - mapRenderSize / 2;
+        int mapStartY = mapY - mapRenderSize / 2;
+
+        ChunkStatus prevStatus = null;
+        int prevColor = NULL_STATUS_COLOR;
+
+        for (int x = 0; x < size; ++x) {
+            int tileX = mapStartX + x * tileSize;
+
+            for (int z = 0; z < size; ++z) {
+                int tileY = mapStartY + z * tileSize;
+
+                ChunkStatus status = tracker.getChunkStatus(x, z);
+                int color;
+
+                if (prevStatus == status) {
+                    color = prevColor;
+                } else {
+                    color = STATUS_TO_COLOR_FAST.get(status);
+
+                    prevStatus = status;
+                    prevColor = color;
+                }
+
+                addRect(buffer, tileX, tileY, tileX + mapScale, tileY + mapScale, color);
+            }
+        }
+
+        tessellator.draw();
+
+        GlStateManager.enableTexture();
+        GlStateManager.disableBlend();
+    }
+
+    private static void addRect(BufferBuilder buffer, double x1, double y1, double x2, double y2, int color) {
+        float a = (float) (color >> 24 & 255) / 255.0F;
+        float r = (float) (color >> 16 & 255) / 255.0F;
+        float g = (float) (color >> 8 & 255) / 255.0F;
+        float b = (float) (color & 255) / 255.0F;
+        buffer.vertex(x1, y2, 0.0D).color(r, g, b, a).next();
+        buffer.vertex(x2, y2, 0.0D).color(r, g, b, a).next();
+        buffer.vertex(x2, y1, 0.0D).color(r, g, b, a).next();
+        buffer.vertex(x1, y1, 0.0D).color(r, g, b, a).next();
+    }
+}
